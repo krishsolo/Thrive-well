8c8605a7d4ae9cca8e17b184f7c53975f9849db4
krishsolo <122010416+krishsolo@users.noreply.github.com>
2024-11-08 12:32:11 +0545

    added script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Script to add functionality, e.g., button click events
+document.querySelector('.btn-signin').addEventListener('click', function() {
+    alert('Sign In button clicked!');
+});
+
+document.querySelector('.btn-contact').addEventListener('click', function() {
+    alert('Contact Us button clicked!');
+});
